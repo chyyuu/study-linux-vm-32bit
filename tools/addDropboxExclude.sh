f7fd934addd5ec7e989449136991bb98a7dfedd4
ziyang <ziyang3721@gmail.com>
2015-01-28 14:53:57 +0800

    dropbox exclude needs wildcard file name

--- tools/addDropboxExclude.sh
+++ tools/addDropboxExclude.sh
@@ -8,23 +8,13 @@ do
         #line include "#" is comment
         if [[ $LINE != *"#"* ]]
         then
-            # wildcard * need to find 
-            if [[ $LINE == *"*"* ]]
-            then
-                files=($(find -name $LINE ))
+            files=($(find -name $LINE ))
 
-                #file lists
-                for file in "${files[@]}"; do
-                    cmd="dropbox exclude add $file"
-                    eval "$cmd" 
-                done
-            else
-                #specified file can't use command find
-                #there may be file with samename
-                cmd="dropbox exclude add $LINE"
+            #file lists
+            for file in "${files[@]}"; do
+                cmd="dropbox exclude add $file"
                 eval "$cmd" 
-            fi  
-
+            done
         fi
     fi
 done < .gitignore
